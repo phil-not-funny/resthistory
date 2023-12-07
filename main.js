da75c182b27fb4e5a582b478ba1f9a37ee84b2f8
SCH22538 <SCH22538@spengergasse.at>
2023-12-07 16:30:38 +0100

    sqlite update

--- main.js
+++ main.js
@@ -5,8 +5,12 @@ const sequelize = require("./database");
 const { createCountries } = require("./context");
 const Country = require("./model/Country");
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({
+  //
+  force: true
+}).then(() => {
   console.log("Database ready!");
+  //NOTE - Only enable when regenerating 
   createCountries();
 });
 
@@ -14,11 +18,11 @@ const PORT = process.env.PORT || 3100;
 
 //STUB - Routes
 
-app.get("/all", async (req, res) => {
+app.get("/country/all", async (req, res) => {
   res.send(await Country.findAll({}));
 });
 
-app.get("/country/:name", async (req, res) => {
+app.get("/country/name/:name", async (req, res) => {
   res.send(
     await Country.findOne({
       where: {
@@ -26,6 +30,33 @@ app.get("/country/:name", async (req, res) => {
       },
     })
   );
+});
+
+app.get("/country/id/:id", async (req, res) => {
+  res.send(
+    await Country.findOne({
+      where: {
+        id: req.params.id,
+      },
+    })
+  );
+});
+
+app.get("/country/random", async (req, res, next) => {
+  let rarity = Math.floor(Math.random() * 99) + 1;
+
+  if (rarity <= 40) rarity = 1;
+  else if (rarity <= 70) rarity = 2;
+  else if (rarity <= 85) rarity = 3;
+  else if (rarity <= 93) rarity = 4;
+  else if (rarity <= 98) rarity = 5;
+  else rarity = 6;
+
+  const countries = await Country.findAll({ where: { rarity: rarity } });
+  let country = countries[Math.floor(Math.random() * countries.length)];
+
+  if (country) res.send(country);
+  else res.status(400).json({ error: "No Country found" });
 });
 
 app.listen(PORT, () => {
